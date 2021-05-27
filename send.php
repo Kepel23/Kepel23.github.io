f6d0a95122e6746085291910d4583f34f4f4f20e
Anton25 <anton_sergeev_456@mail.ru>
2021-05-27 16:30:53 +0300

    Initial commit

---
+++ send.php
@@ -0,0 +1,20 @@
+<?php
+    //проверяем, существуют ли переменные в массиве POST
+    if (!isset($_POST['fio']) and !isset($_POST['email'])) {
+        } else {
+                //показываем форму
+                $fio = $_POST['fio'];
+                $email = $_POST['email'];
+                $fio = htmlspecialchars($fio);
+                $email = htmlspecialchars($email);
+                $fio = urldecode($fio);
+                $email = urldecode($email);
+                $fio = trim($fio);
+                $email = trim($email);
+                if (mail("anton_sergeev_456@mail.ru@mail.ru", "Заявка с сайта", "ФИО:" . $fio . ". E-mail: " . $email, "From: example2@mail.ru \r\n")) {
+                    echo "Сообщение успешно отправлено";
+                } else {
+                    echo "При отправке сообщения возникли ошибки";
+                }
+            }
+?>
